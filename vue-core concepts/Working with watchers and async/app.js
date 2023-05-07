8389e50e2a9faa7ed9b74f33e31ddf8da0e910ea
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-05-07 12:10:03 +0200

    Working with Watchers and async functions: Now the query only is fetched, and answer and image are shown if the '?' sign is encountered. If there's nothin on the question input both contents disappear

--- vue-core concepts/Working with watchers and async/app.js
+++ vue-core concepts/Working with watchers and async/app.js
@@ -1,26 +1,33 @@
 Vue.createApp({
-    data(){
-        return {
-            question:'',
-            answer:'',
-            title:'Learning watchers'
-        }
+  data() {
+    return {
+      question: "",
+      answer: "",
+      title: "Learning watchers",
+      image: "",
+    };
+  },
+  watch: {
+    question(currentQuestion) {
+      if (currentQuestion.indexOf("?") > -1) {
+        this.getAnswer();
+      } else {
+        !this.question && (this.answer = '');
+        !this.question && (this.image = '');
+      }
     },
-    watch: {
-        question(currentQuestion) {
-            (currentQuestion.indexOf('?') > -1) && this.getAnswer()
-        }
+  },
+  methods: {
+    async getAnswer() {
+      this.answer = "Loading";
+      try {
+        const result = await fetch("https://yesno.wtf/api");
+        const fetchedObj = await result.json();
+        this.answer = fetchedObj.answer;
+        this.image = fetchedObj.image;
+      } catch (err) {
+        this.answer = `Sorry. It wasnt possible to find your answer. ${err}`;
+      }
     },
-    methods: {
-        async getAnswer () {
-            this.answer = 'Loading';
-            try {
-                const result = await fetch('https://yesno.wtf/api');
-                this.answer = (await result.json()).answer;
-
-            } catch (err) {
-                this.answer = `Sorry. It wasnt possible to finde your answer. ${err}`
-            }
-        }
-    },
-}).mount('#app');
+  },
+}).mount("#app");
