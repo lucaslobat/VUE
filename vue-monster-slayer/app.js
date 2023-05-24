e5e45f7a9369747c156d6fa368060a796a3e6f00
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-05-24 10:29:26 +0200

    Working on a new project to crystalise the core concepts and put tem in practice

---
+++ vue-monster-slayer/app.js
@@ -0,0 +1,17 @@
+Vue.createApp(
+    {
+        data() {
+            return  {
+                playerHealth:100,
+                monsterHealth:200,
+                attackDamage:0,
+                
+            }
+        },
+        methods: {
+            attack(){
+
+            }
+        }
+    }
+).mount('#game');
