fba54fcc222d676287550829b22d4ea90847426b
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-05-26 09:56:03 +0200

    Implementing the attack function, now when we attack the monster's healthbar decreases!

--- vue-monster-slayer/app.js
+++ vue-monster-slayer/app.js
@@ -2,14 +2,22 @@ Vue.createApp(
     {
         data() {
             return  {
-                playerHealth:100,
-                monsterHealth:200,
+                playerHealth:`${100}%`,
+                monsterHealth:100,
                 attackDamage:0,
+                battleLog: []
                 
             }
         },
         methods: {
             attack(){
+                this.attackDamage = Math.floor(Math.random() * 20) + 1;
+                let parsedMonsterHealth = parseFloat(this.monsterHealth);
+
+                let newMonsterHealth = parsedMonsterHealth - this.attackDamage;
+                this.monsterHealth = `${newMonsterHealth}%`;
+
+                console.log(this.monsterHealth)
 
             }
         }
