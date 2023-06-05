14e5be5b3c2e4f15371f9987520018f3969e6951
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-06-05 09:43:36 +0200

    The special attack only works if any of the characters are alive

--- vue-monster-slayer/app.js
+++ vue-monster-slayer/app.js
@@ -1,4 +1,4 @@
-const genRandomNumber = (min,max) => {
+const genRandomNumber = (min, max) => {
   let randomNumber = Math.floor(Math.random() * max) + min;
   return randomNumber;
 };
@@ -9,21 +9,20 @@ Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       battleLog: [],
-      gameOverMessage:undefined,
-      roundCounter:0
+      gameOverMessage: undefined,
+      roundCounter: 0,
     };
   },
   methods: {
     playerAttack() {
       console.log(this.roundCounter % 3);
       if (this.playerHealth > 0) {
-
         this.roundCounter++;
 
         //Damage action
-        const playerAttackDamage = genRandomNumber(5,20);
+        const playerAttackDamage = genRandomNumber(5, 20);
         this.monsterHealth -= playerAttackDamage;
-        
+
         //Log
         this.battleLog.push({
           text: `The player damaged the monster by ${playerAttackDamage}`,
@@ -32,43 +31,47 @@ Vue.createApp({
         //Strike back
         this.monsterAttack();
       } else {
-        this.gameOverMessage = "You're dead."
+        this.gameOverMessage = "You're dead.";
       }
     },
     monsterAttack() {
       if (this.monsterHealth > 0) {
-        const monsterAttackDamage = genRandomNumber(8,25);
+        const monsterAttackDamage = genRandomNumber(8, 25);
         this.playerHealth -= monsterAttackDamage;
 
         this.battleLog.push({
           text: `The monster damaged the player by ${monsterAttackDamage}`,
         });
       } else {
-        this.gameOverMessage = "The monster has been slayed."
+        this.gameOverMessage = "The monster has been slayed.";
       }
     },
-    specialAttack(){
-      this.roundCounter++;
-      const specialAttack = genRandomNumber(10,30);
-      this.monsterHealth -= specialAttack;
+    specialAttack() {
+      if (this.playerHealth > 0 || this.monsterHealth > 0) {
+        this.roundCounter++;
+        const specialAttack = genRandomNumber(10, 30);
+        this.monsterHealth -= specialAttack;
 
-      this.battleLog.push({
-        text: `The player damaged the monster by ${specialAttack}`,
-      });
+        this.battleLog.push({
+          text: `The player damaged the monster by ${specialAttack}`,
+        });
 
-      this.monsterAttack();
-
-    }
+        this.monsterAttack();
+      }
+    },
   },
   computed: {
     dynamicWidthMonster() {
       return { width: `${this.monsterHealth}%` };
     },
-    dynamicWidthPlayer(){
-        return { width: `${this.playerHealth}%` };
+    dynamicWidthPlayer() {
+      return { width: `${this.playerHealth}%` };
     },
-    useSpecialAttack(){
-      return ((this.roundCounter % 3 !== 0) || (this.roundCounter === 0) && this.gameOverMessage != undefined)
-    }
+    useSpecialAttack() {
+      return (
+        this.roundCounter % 3 !== 0 ||
+        (this.roundCounter === 0 && this.gameOverMessage != undefined)
+      );
+    },
   },
 }).mount("#game");
