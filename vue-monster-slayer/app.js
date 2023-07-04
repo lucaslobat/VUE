757ea1956065d2df6abbc03a69c5f1d6aa5f4d7c
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-07-04 13:28:53 +0200

    added a win condition and a message depending on which player has won or if the game has ended in a draw.

--- vue-monster-slayer/app.js
+++ vue-monster-slayer/app.js
@@ -6,44 +6,75 @@ const genRandomNum = (min, max) => {
 Vue.createApp({
   data() {
     return {
-      playerHealth: 100,
+      // Variables utilizadas para controlar la vida del jugador y del monstruo y la ronda
+      heroHealth: 100,
       monsterHealth: 100,
       roundCounter: 1,
+      battleLog: [],
+      gameOverMessage: null,
     };
   },
   methods: {
-    playerAttack() {
+    heroAttack() {
       this.roundCounter++;
       const attackDamage = genRandomNum(5, 16);
-      this.monsterHealth -= attackDamage;
+      this.monsterHealth - attackDamage <= 0
+        ? (this.monsterHealth = 0)
+        : (this.monsterHealth -= attackDamage);
+      this.battleLog.push(`The hero inflicted ${attackDamage} on the monster`);
       this.monsterAttack();
     },
     monsterAttack() {
       const attackDamage = genRandomNum(5, 21);
-      this.playerHealth -= attackDamage;
+      this.heroHealth - attackDamage <= 0
+        ? (this.heroHealth = 0)
+        : (this.heroHealth -= attackDamage);
+      this.battleLog.push(`The monster inflicted ${attackDamage} on the hero`);
     },
     specialAttack() {
       this.roundCounter++;
       const attackDamage = genRandomNum(5, 16) * 2;
-      this.monsterHealth -= attackDamage;
+      this.monsterHealth - attackDamage <= 0
+        ? (this.monsterHealth = 0)
+        : (this.monsterHealth -= attackDamage);
+      this.battleLog.push(
+        `The hero inflicted a special attack of ${attackDamage} on the monster`
+      );
     },
-    healPlayer() {
+    healHero() {
       this.roundCounter++;
       const healValue = genRandomNum(8, 20);
-      this.playerHealth + healValue > 100
-        ? (this.playerHealth = 100)
-        : (this.playerHealth += healValue);
+      this.heroHealth + healValue > 100
+        ? (this.heroHealth = 100)
+        : (this.heroHealth += healValue);
+      this.battleLog.push(`The hero healed for ${healValue}`);
     },
   },
   computed: {
     monsterBarWidth() {
       return { width: `${this.monsterHealth}%` };
     },
-    playerBarWidth() {
-      return { width: `${this.playerHealth}%` };
+    heroBarWidth() {
+      return { width: `${this.heroHealth}%` };
     },
     activateSpecialAttack() {
       return this.roundCounter % 3 !== 0;
+    },
+  },
+  watch: {
+    heroHealth(hHealth) {
+      if (hHealth <= 0 && this.monsterHealth <= 0) {
+        this.gameOverMessage = "The game ended in a draw";
+      } else if (this.monsterHealth <= 0) {
+        this.gameOverMessage = "You win!";
+      }
+    },
+    monsterHealth(mHealth) {
+      if (mHealth <= 0 && this.heroHealth <= 0) {
+        this.gameOverMessage = "The game ended in a draw";
+      } else if (this.heroHealth <= 0) {
+        this.gameOverMessage = "You lose.";
+      }
     },
   },
 }).mount("#game");
