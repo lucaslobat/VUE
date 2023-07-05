9d4b96538e9430236098d155e66c6a3b93c9b97d
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-07-05 11:24:14 +0200

    All button functionalities were finished. Used a condition on computed properties to check if the hero and monster health are below 0 and then set the health bar width to 0

--- vue-monster-slayer/app.js
+++ vue-monster-slayer/app.js
@@ -15,28 +15,29 @@ Vue.createApp({
     };
   },
   methods: {
+    newGame() {
+      this.heroHealth = 100;
+      this.monsterHealth = 100;
+      this.roundCounter = 0;
+      this.battleLog = [];
+      this.gameOverMessage = null;
+    },
     heroAttack() {
       this.roundCounter++;
       const attackDamage = genRandomNum(5, 16);
-      this.monsterHealth - attackDamage <= 0
-        ? (this.monsterHealth = 0)
-        : (this.monsterHealth -= attackDamage);
+      this.monsterHealth -= attackDamage;
       this.battleLog.push(`The hero inflicted ${attackDamage} on the monster`);
       this.monsterAttack();
     },
     monsterAttack() {
       const attackDamage = genRandomNum(5, 21);
-      this.heroHealth - attackDamage <= 0
-        ? (this.heroHealth = 0)
-        : (this.heroHealth -= attackDamage);
+      this.heroHealth -= attackDamage;
       this.battleLog.push(`The monster inflicted ${attackDamage} on the hero`);
     },
     specialAttack() {
       this.roundCounter++;
       const attackDamage = genRandomNum(5, 16) * 2;
-      this.monsterHealth - attackDamage <= 0
-        ? (this.monsterHealth = 0)
-        : (this.monsterHealth -= attackDamage);
+      this.monsterHealth -= attackDamage;
       this.battleLog.push(
         `The hero inflicted a special attack of ${attackDamage} on the monster`
       );
@@ -49,12 +50,21 @@ Vue.createApp({
         : (this.heroHealth += healValue);
       this.battleLog.push(`The hero healed for ${healValue}`);
     },
+    surrender() {
+      this.gameOverMessage = "You've surrended."
+    }
   },
   computed: {
     monsterBarWidth() {
+      if (this.monsterHealth < 0) {
+        return { width: `0%` };
+      }
       return { width: `${this.monsterHealth}%` };
     },
     heroBarWidth() {
+      if (this.heroHealth < 0) {
+        return { width: `0%` };
+      }
       return { width: `${this.heroHealth}%` };
     },
     activateSpecialAttack() {
@@ -65,15 +75,15 @@ Vue.createApp({
     heroHealth(hHealth) {
       if (hHealth <= 0 && this.monsterHealth <= 0) {
         this.gameOverMessage = "The game ended in a draw";
-      } else if (this.monsterHealth <= 0) {
-        this.gameOverMessage = "You win!";
+      } else if (hHealth <= 0) {
+        this.gameOverMessage = "You lose.";
       }
     },
     monsterHealth(mHealth) {
       if (mHealth <= 0 && this.heroHealth <= 0) {
         this.gameOverMessage = "The game ended in a draw";
-      } else if (this.heroHealth <= 0) {
-        this.gameOverMessage = "You lose.";
+      } else if (mHealth <= 0) {
+        this.gameOverMessage = "You win! The monster has been slain";
       }
     },
   },
