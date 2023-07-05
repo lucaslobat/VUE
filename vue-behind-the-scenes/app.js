478825aa0099668cd2119ab9ec7f2d77ec2e4c9c
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-07-05 13:34:39 +0200

    Using lifecycle hooks

--- vue-behind-the-scenes/app.js
+++ vue-behind-the-scenes/app.js
@@ -8,9 +8,28 @@ const app = Vue.createApp({
   methods: {
     setText() {
       this.message = this.$refs.userInputText.value;
-      console.log(this.$refs.userInputText.value)
     },
   },
+
+beforeCreate(){
+  console.log("beforeCreate()")
+},
+created(){
+  console.log("created()")
+},
+beforeMount(){
+  console.log("beforeMount()");
+},
+mounted(){
+  console.log("mounted()");
+},
+beforeUpdate(){
+  console.log("beforeUpdate()");
+},
+updated(){
+  console.log("updated()");
+}
+
 });
 
 app.mount('#app');
