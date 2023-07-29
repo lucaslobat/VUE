bb9cf70ff3bf6f1de4441a397333c058b576b864
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-07-29 12:31:12 +0200

    Practising componente registration: global components and local components

---
+++ badge-system-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <TheHeader />
+    <badge-list></badge-list>
+    <UserInfo
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    />
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  // Componentes importados localmente
+  components: { TheHeader, UserInfo },
+
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
