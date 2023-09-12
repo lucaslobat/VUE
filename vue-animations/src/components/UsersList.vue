6fa4e92e57c20aad3956bfc562b4ddba7a8eab1a
Lucas Lobato Botelho <criptoryu@gmail.com>
2023-09-12 20:47:30 +0200

    Working with the <TransitionGroup> component to animate items when adding and removing items from a list

--- vue-animations/src/components/UsersList.vue
+++ vue-animations/src/components/UsersList.vue
@@ -1,29 +1,29 @@
 <template>
-    <ul>
-        <li v-for="user in usersArray" key="user" @click="removeUser(user)"> {{ user }}</li>
-    </ul>
+    <TransitionGroup tag="ul" name="usersList">
+        <li v-for="user in usersArray" :key="user" @click="removeUser(user)"> {{ user }}</li>
+    </TransitionGroup>
     <form @submit.prevent="addUser">
-        <label for="enterUser">Introduce a new user: </label>
-        <input type="text" id="enterUser" ref="enteredUser">
-        <button>Save</button>
+            <label for="enterUser">Introduce a new user: </label>
+            <input type="text" id="enterUser" ref="enteredUser">
+            <button>Save</button>
 
-    </form>
+        </form>
 </template>
 
 <script>
 export default {
-    data(){
+    data() {
         return {
-            usersArray:['Dorothy','Victoria','Mandy','Bridget']
+            usersArray: ['Dorothy', 'Victoria', 'Mandy', 'Bridget']
         }
     },
     methods: {
-        addUser(){
+        addUser() {
             const enteredUser = this.$refs.enteredUser.value;
             this.usersArray.unshift(enteredUser);
             this.$refs.enteredUser.value = "";
         },
-        removeUser(userId){
+        removeUser(userId) {
             const filteredArray = this.usersArray.filter((user) => user !== userId);
             this.usersArray = filteredArray;
         }
@@ -32,16 +32,27 @@ export default {
 </script>
 
 <style scoped>
-
 ul {
     list-style: none;
-    padding:0;
-    margin:0;
+    padding: 0;
+    margin: 0;
 }
 
 li {
     border: 2px solid #ccc;
-    margin:0.5rem;
-    padding:1rem;
+    margin: 0.5rem;
+    padding: 1rem;
+}
+
+.usersList-move,
+.usersList-enter-active,
+.usersList-leave-active {
+    transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.usersList-enter-from,
+.usersList-leave-to {
+    opacity: 0;
+    transform: scaleY(0.01) translate(30px, 0);
 }
 </style>
